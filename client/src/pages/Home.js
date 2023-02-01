3c3c223b76c6508b776be22c703513ea875c8508
john-langlois <joao.langlois@hotmail.com>
2023-02-01 17:45:59 -0500

    adding instructor json data

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+//Page to select either admin or instructor view
+
+
+import React, {useState} from 'react';
+
+const Home = () =>{
+    const[isAdmin, setIsAdmin] = useState();
+
+}
+
+export default Home;
