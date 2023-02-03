ca21e26260501da88200948a724acf4bdcb13313
john-langlois <joao.langlois@hotmail.com>
2023-02-03 14:59:36 -0500

    adding login route & login page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
-
 app.use(express.json());
 
+
+const loginRoute = require('./routes/login.js')
+app.use('/api/login',loginRoute)
+
+const changelogRoute = require('./routes/changelog.js')
+app.use('/api/changelog',changelogRoute)
 
 const PORT  = 5000;
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
