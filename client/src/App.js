81819c7e435be78485a8ac76abddb9fbea3ec827
Khalid <khaltahan@gmail.com>
2023-02-08 17:16:05 -0500

    added courses.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-import { Route, Routes} from 'react-router-dom'
-import OutlineView from './pages/OutlineView';
-import Home from './pages/Home';
-import AdminView from './pages/AdminView'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import OutlineView from "./pages/OutlineView";
+import Home from "./pages/Home";
+import AdminView from "./pages/AdminView";
 
 function App() {
   return (
     <Routes>
-      <Route path = '/' exact element = {<Home/>}/>
-      <Route path = '/outline-view' exact element = {<OutlineView/>}/>
-      <Route path = '/admin-view' exact element = {<AdminView/>}/>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/outline-view" exact element={<OutlineView />} />
+      <Route path="/admin-view" exact element={<AdminView />} />
     </Routes>
-    
   );
 }
 
