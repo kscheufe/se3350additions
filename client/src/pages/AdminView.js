b5a7ec0b56552fa73c0827972a917a78a4183e17
Khalid <khaltahan@gmail.com>
2023-02-13 15:27:47 -0500

    course and instructor attribute front-end display

--- client/src/pages/AdminView.js
+++ client/src/pages/AdminView.js
@@ -2,32 +2,69 @@ import React, { useState, useEffect } from "react";
 
 const AdminView = () => {
   const [courses, setCourses] = useState([]);
-  const [selectedCourse, setSelectedCourse] = useState("");
+  const [selectedCourse, setSelectedCourse] = useState({});
+  const [instructors, setInstructors] = useState([]);
+  const [selectedInstructor, setSelectedInstructor] = useState({});
 
   useEffect(() => {
-    const fetchCourses = async () => {
-      const response = await fetch("http://localhost:5000/api/courses");
-      const data = await response.json();
-      setCourses(data);
+    const fetchData = async () => {
+      const courseResponse = await fetch("http://localhost:5000/api/courses");
+      const courseData = await courseResponse.json();
+      setCourses(courseData);
+
+      const instructorResponse = await fetch("http://localhost:5000/api/getinstructors");
+      const instructorData = await instructorResponse.json();
+      setInstructors(instructorData);
     };
 
-    fetchCourses();
+    fetchData();
   }, []);
 
-  const handleChange = (event) => {
-    setSelectedCourse(event.target.value);
+  const handleCourseChange = (event) => {
+    const selectedCourse = courses.find((course) => course.name === event.target.value);
+    setSelectedCourse(selectedCourse || {});
+  };
+
+  const handleInstructorChange = (event) => {
+    const selectedInstructor = instructors.find((instructor) => instructor.name === event.target.value);
+    setSelectedInstructor(selectedInstructor || {});
   };
 
   return (
     <div>
       <h2>Select a Course:</h2>
-      <select value={selectedCourse} onChange={handleChange}>
+      <select value={selectedCourse.name} onChange={handleCourseChange}>
         {courses.map((course) => (
-          <option key={course} value={course}>
-            {course}
+          <option key={course.name} value={course.name}>
+            {course.name}
           </option>
         ))}
       </select>
+      <h2>Select an Instructor:</h2>
+      <select value={selectedInstructor.name} onChange={handleInstructorChange}>
+        {instructors.map((instructor) => (
+          <option key={instructor.name} value={instructor.name}>
+            {instructor.name}
+          </option>
+        ))}
+      </select>
+      {selectedInstructor.name ? (
+        <div>
+          <h2>Selected Instructor:</h2>
+          <p>Name: {selectedInstructor.name}</p>
+          <p>Email: {selectedInstructor.email}</p>
+          <p>Specialty: {selectedInstructor.specialty}</p>
+        </div>
+      ) : null}
+      {selectedCourse.name ? (
+        <div>
+          <h2>Selected Course:</h2>
+          <p>Name: {selectedCourse.name}</p>
+          <p>Description: {selectedCourse.description}</p>
+          <p>Instructor: {selectedCourse.instructor}</p>
+          <p>Duration: {selectedCourse.duration}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
--- server/routes/getCourses.js
+++ server/routes/getCourses.js
@@ -5,17 +5,7 @@ const coursesData = require("../data/courses.json");
 const courses = coursesData.courses;
 
 const getCourseCodes = (req, res) => {
-  let courseCodes = [];
-
-  for (let i = 0; i < courses.length; i++) {
-    let courseString = courses[i].name + " " + courses[i].code;
-    courseCodes.push(courseString);
-  }
-
-  console.log(courses.length);
-  console.log(courseCodes);
-
-  return res.send(courseCodes);
+  return res.send(courses);
 };
 
 router.route("/").get(getCourseCodes);
