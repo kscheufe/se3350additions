cba7cbd1b1ba4da194717c4ff128d8d67bc7a1c6
Khalid <khaltahan@gmail.com>
2023-02-13 23:49:51 -0500

    added styling for admin view

--- client/src/pages/AdminView.js
+++ client/src/pages/AdminView.js
@@ -1,4 +1,6 @@
+import "../styles/AdminView.css";
 import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const AdminView = () => {
   const [courses, setCourses] = useState([]);
@@ -50,40 +52,55 @@ const AdminView = () => {
   };
 
   return (
-    <div>
-      <h2>Select a Course:</h2>
-      <select value={selectedCourse.name} onChange={handleCourseChange}>
-        {courses.map((course) => (
-          <option key={course.name} value={course.name}>
-            {course.name}
-          </option>
-        ))}
-      </select>
-      <h2>Select an Instructor:</h2>
-      <select value={selectedInstructor.name} onChange={handleInstructorChange}>
-        {instructors.map((instructor) => (
-          <option key={instructor.name} value={instructor.name}>
-            {instructor.name}
-          </option>
-        ))}
-      </select>
-      {selectedInstructor.name ? (
-        <div>
-          <h2>Selected Instructor:</h2>
-          <p>ID: {selectedInstructor.ID}</p>
-          <p>Name: {selectedInstructor.name}</p>
-          <p>Email: {selectedInstructor.email}</p>
-          <p>Assigned Courses: {selectedInstructor.assigned_courses}</p>
+    <div className="div-structure">
+      <div className="navbar-admin">
+        <Navbar />
+      </div>
+      <div className="div-selection">
+        <div className="instructor-selection">
+          <h2>Select an Instructor:</h2>
+          <select value={selectedInstructor.name} onChange={handleInstructorChange}>
+            {instructors.map((instructor) => (
+              <option key={instructor.name} value={instructor.name}>
+                {instructor.name}
+              </option>
+            ))}
+          </select>
         </div>
-      ) : null}
-      {selectedCourse.name ? (
-        <div>
-          <h2>Selected Course:</h2>
-          <p>Code: {selectedCourse.code}</p>
-          <p>Name: {selectedCourse.name}</p>
-          <p>Description: {selectedCourse.description}</p>
+        <div className="course-selection">
+          <h2>Select a Course:</h2>
+          <select value={selectedCourse.name} onChange={handleCourseChange}>
+            {courses.map((course) => (
+              <option key={course.name} value={course.name}>
+                {course.name}
+              </option>
+            ))}
+          </select>
         </div>
-      ) : null}
+        {/* </div> */}
+        {/* <div className="div-selection"> */}
+      </div>
+      <div className="information-structure">
+        <div className="information">
+          {selectedInstructor.name ? (
+            <div className="instructor-info">
+              <h2>Selected Instructor:</h2>
+              <p>ID: {selectedInstructor.ID}</p>
+              <p>Name: {selectedInstructor.name}</p>
+              <p>Email: {selectedInstructor.email}</p>
+              <p>Assigned Courses: {selectedInstructor.assigned_courses}</p>
+            </div>
+          ) : null}
+          {selectedCourse.name ? (
+            <div className="course-info">
+              <h2>Selected Course:</h2>
+              <p>Code: {selectedCourse.code}</p>
+              <p>Name: {selectedCourse.name}</p>
+              <p>Description: {selectedCourse.description}</p>
+            </div>
+          ) : null}
+        </div>
+      </div>
       <br />
       <br />
       <button onClick={assignCourseToInstructor}>Assign Course to Instructor</button>
