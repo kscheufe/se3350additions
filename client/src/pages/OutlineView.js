783078f0b7b0874836eca4d0cbe8fd7ac334ebe1
john-langlois <joao.langlois@hotmail.com>
2023-02-23 00:41:35 -0500

    add editor page for each assigned course

--- client/src/pages/OutlineView.js
+++ client/src/pages/OutlineView.js
@@ -1,26 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import Container from '../components/Navbar/Container';
 import '../styles/OutlineView.css'
+import {Link, Outlet, useParams} from 'react-router-dom';
 
 
-const OutlineView = () => {
-    const[courses,setCourses] = useState([])
 
-    const user = localStorage.getItem('user')
-    console.log(JSON.parse(user))
+const OutlineView = ({courses}) => {
+    let params = useParams();
+    const length = Object.keys(params).length;
+    let outlineView;
+    if(length === 0){
+        outlineView = true;
+    }
+    else{
+        outlineView = false;
+    }
 
-
+        //if no courses have been assigned it will display a message
+        let assigned = courses.length > 0;
     return(
         <div>
+            {outlineView && <>
             <Navbar/>
-        <div className="content"> 
-        <h1>View and Edit Course Outline Here</h1>
-            <div className = "courses-grid">
-                <Container/>
-
+            <h1>View and Edit Course Outline Here</h1>
+            <div className = "courses-flex">
+                        {assigned ? courses.map((course, index)=>(
+                                    <div key = {index}  className = "courses-item">
+                                        <Link to = {`/outline-view/${course}`} >
+                                            <h2>{course}</h2>
+                                            <p>Navigate to Outline Editor</p>
+                                        </Link>
+                                    </div>
+                                    
+                            )): <h2>You have not yet been assigned a course</h2>}
             </div>
-        </div>
+            </>}
+            
+            {!outlineView && <Outlet/> }
         </div>
     )
 }
