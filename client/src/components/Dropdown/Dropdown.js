ef50f0e9097024dbd12288f594f8b856c2186765
xiajeremy <jxia47@uwo.ca>
2023-03-24 19:38:50 -0400

    Edited dropdown component to fix bugs

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,30 +1,22 @@
 import React, { useState } from 'react';
 
 const Dropdown = ({ options, onSelect }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState('');
 
   const handleSelect = (option) => {
     setSelectedOption(option);
     onSelect(option);
-    setIsOpen(false);
   };
 
   return (
-    <div className="dropdown">
-      <button onClick={() => setIsOpen(!isOpen)}>
-        {selectedOption ? selectedOption.label : 'Select an option'}
-      </button>
-      {isOpen && (
-        <ul className="dropdown-menu">
-          {options.map((option) => (
-            <li key={option.value} onClick={() => handleSelect(option)}>
-              {option.label}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <select value={selectedOption} onChange={(e) => handleSelect(e.target.value)}>
+      <option value="">Select an option</option>
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
   );
 };
 
